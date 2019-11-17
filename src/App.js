3ff6f4122ab284d6f1eec153095a7adda19c5950
Sachin796 <sachinjaveri.2010@gmail.com>
2019-11-17 13:25:59 -0500

    Clicky game react

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+import ClickGameContainer from "./components/ClickGameContainer"
+
+function App() {
+  return <ClickGameContainer />;
+}
+export default App;
